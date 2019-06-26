1a204b5c0ecfdb0d1c456b050006e5417cc78cae
kavya <kavyae05@gmail.com>
2019-06-26 19:11:56 +0530

    committing

---
+++ demogit/src/a/demo1.java
@@ -0,0 +1,13 @@
+package a;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo1 {
+	@Test
+	public void test()
+	{
+Reporter.log("hello",true);
+	}
+
+}
---
+++ demogit/src/a/demo2.java
@@ -0,0 +1,13 @@
+package a;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo2 {
+	@Test
+	public void test()
+	{
+Reporter.log("hi",true);
+	}
+
+}
